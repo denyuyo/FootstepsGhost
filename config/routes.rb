a9b93229e42edeac4d2b405b1d9c48f0fb255784
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-03 07:15:45 +0000

    README.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
- 
+ # ユーザー用
+# URL /users/sign_in ...
+devise_for :users, skip: [:passwords],  controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
   
 end
